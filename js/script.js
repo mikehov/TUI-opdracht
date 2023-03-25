fb05b9a2c40e76093bc3ac736468524777febd39
Mike <mikehovje@gmail.com>
2023-03-26 00:46:23 +0100

    Results almost done

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// console.log("Script works");
+
+// const slider1 = document.getElementById("slider-1");
+// const slider2 = document.getElementById("slider-2");
+
+// slider1.addEventListener("input", updateSlider);
+// slider2.addEventListener("input", updateSlider);
+
+// function updateSlider() {
+//   const value1 = parseInt(slider1.value);
+//   const value2 = parseInt(slider2.value);
+//   if (value1 >= value2) {
+//     slider1.value = value2 - 1;
+//   }
+//   if (value2 <= value1) {
+//     slider2.value = value1 + 1;
+//   }
+// }
